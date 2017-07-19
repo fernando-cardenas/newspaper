016a7320d87b7c9bc742558b6eadf146ad3b46dd
Fernando Cardenas <fernando.cardenas.fernandez@gmail.com>
2017-07-20 00:29:30 +0200

    Clase 1: Configurado setting para poder conectarse desde cualquier máquina a la IP 192.168.1.15

--- newspaper/newspaper/settings.py
+++ newspaper/newspaper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i8_uwvul8(*+t904*sqy#+kp#gl+lt_b*$a*4^o_1yn^e5!%=x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [u'192.168.1.15']
 
 
 # Application definition
