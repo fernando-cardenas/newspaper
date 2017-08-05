889edde7821864b534c384e01a59361d8ab153f0
Fernando Cardenas <fernando.cardenas.fernandez@gmail.com>
2017-08-06 00:21:48 +0200

    Clase 2: Añadimos administración de noticias

--- newspaper/newspaper/news/admin.py
+++ newspaper/newspaper/news/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from newspaper.news.models import News
 
 # Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(News, NewsAdmin)
