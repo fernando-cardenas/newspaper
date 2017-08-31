8de96c84139602f3c816144089d3f373ea0bba09
Fernando Cardenas <fernando.cardenas.fernandez@gmail.com>
2017-09-01 00:19:52 +0200

    Clase 2: Primera version de la vista de noticias (listado)

--- newspaper/newspaper/news/views.py
+++ newspaper/newspaper/news/views.py
@@ -1,9 +1,14 @@
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from datetime import datetime
 #from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from newspaper.news.models import News
 
 def news_list(request):
 #    return HttpResponse("Esta es mi primera respuesta")
-    return render_to_response('news/news_list.html')
+    news =  News.objects.filter(
+        publish_date__lte=datetime.now()).order_by('-publish_date')
+    return render_to_response('news/news_list.html',
+                              {'news': news})
